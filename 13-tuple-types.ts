d00ccd37842fe8570c1d002a67cda467489afb9d
ivandoza <ivandoza@gmail.com>
2024-02-05 11:34:37 +0000

    exercises 11 to 15

--- 13-tuple-types.ts
+++ 13-tuple-types.ts
@@ -4,22 +4,22 @@
 
 // Add a tuple type annotation for this tuple.
 
-const countryPopulation = ["China", 1_412_600_000];
+const countryPopulation: [string, number] = ["China", 1_412_600_000];
 
 // Change the `CountryWithPopulation` type into a tuple type.
 // This should fix the type error on Line 14.
 
-type CountryWithCurrency = [];
+type CountryWithCurrency = [string, string, string];
 
-const countryCurrency: CountryWithCurrency = ["Italy", "Euro", "EUR"];
+const countryCurrency: Array<CountryWithCurrency> = [["Italy", "Euro", "EUR"]];
 
 // Add a tuple type annotation that prevents this tuple from being modified.
 // Use the readonly modifier and the tuple type syntax: readonly [type, type]
 // This will cause a type error. Remove the code that now has a type error.
 
-const countryLanguage = ["Greece", "Greek"];
+const countryLanguage: readonly [string, string] = ["Greece", "Greek"];
 
-countryLanguage.reverse();
+// countryLanguage.reverse();
 
 console.log(countryLanguage);
 
